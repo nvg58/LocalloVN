5c2a212d9128e442236c7818713b1a65be7bb262
Giap Nguyen <nvangiap5894@gmail.com>
2015-05-01 21:24:01 +0700

    Finish Walkthrough activity

--- app/src/main/java/com/locol/locol/SplashScreenActivity.java
+++ app/src/main/java/com/locol/locol/SplashScreenActivity.java
@@ -7,11 +7,20 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.ProgressBar;
 
+import com.locol.locol.pojo.Account;
+import com.parse.ParseAnonymousUtils;
+import com.parse.ParseUser;
+
 
 public class SplashScreenActivity extends ActionBarActivity {
 
+    public static final String PREF_FILE_NAME = "user_info";
+    public static final String KEY_USER_ID = "id";
+    public static final String KEY_USER_NAME = "name";
+    public static final String KEY_USER_EMAIL = "email";
+    public static final String KEY_USER_AVATAR = "avatar";
 
-    private static final int TIME = 4 * 1000; // 4 seconds
+    private static final int TIME = 3 * 1000; // 3 seconds
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +34,27 @@ public class SplashScreenActivity extends ActionBarActivity {
             @Override
             public void run() {
                 progressBar.setVisibility(View.GONE);
+
+                if (!ParseAnonymousUtils.isLinked(ParseUser.getCurrentUser())) {
+                    // If current user is NOT anonymous user
+                    // Get current user data from Parse.com
+                    ParseUser currentUser = ParseUser.getCurrentUser();
+                    if (currentUser != null) {
+                        // Send logged in users to MyNavigationDrawer.class
+                        Account.setUserFBId(Preferences.readFromPreferences(SplashScreenActivity.this, PREF_FILE_NAME, KEY_USER_ID, null));
+                        Account.setUserEmail(Preferences.readFromPreferences(SplashScreenActivity.this, PREF_FILE_NAME, KEY_USER_EMAIL, null));
+                        Account.setUserName(Preferences.readFromPreferences(SplashScreenActivity.this, PREF_FILE_NAME, KEY_USER_NAME, null));
+
+                        startActivity(new Intent(SplashScreenActivity.this, MyNavigationDrawer.class));
+                        finish();
+                    } else {
+                        // Send user to LoginActivity.class
+                        Intent intent = new Intent(SplashScreenActivity.this, WalkthroughActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                }
+
 
                 Intent intent = new Intent(SplashScreenActivity.this, WalkthroughActivity.class);
                 startActivity(intent);
@@ -49,5 +79,19 @@ public class SplashScreenActivity extends ActionBarActivity {
     public void onBackPressed() {
         this.finish();
         super.onBackPressed();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
     }
 }
--- app/src/main/java/com/locol/locol/WalkthroughActivity.java
+++ app/src/main/java/com/locol/locol/WalkthroughActivity.java
@@ -1,70 +1,87 @@
 package com.locol.locol;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.View;
 
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.ImageLoader;
-import com.facebook.AccessTokenTracker;
-import com.facebook.CallbackManager;
-import com.facebook.login.widget.LoginButton;
-import com.locol.locol.networks.VolleySingleton;
-import com.locol.locol.pojo.Account;
-import com.parse.ParseAnonymousUtils;
-import com.parse.ParseUser;
 
+public class WalkthroughActivity extends FragmentActivity {
 
-public class WalkthroughActivity extends ActionBarActivity {
+    /**
+     * The number of pages (wizard steps) to show in this demo.
+     */
+    private static final int NUM_PAGES = 3;
 
-    public static final String PREF_FILE_NAME = "user_info";
-    public static final String KEY_USER_ID = "id";
-    public static final String KEY_USER_NAME = "name";
-    public static final String KEY_USER_EMAIL = "email";
-    public static final String KEY_USER_AVATAR = "avatar";
+    /**
+     * The pager widget, which handles animation and allows swiping horizontally to access previous
+     * and next wizard steps.
+     */
+    private ViewPager mPager;
 
-    private LoginButton loginButton;
-    private CallbackManager callbackManager;
-    private AccessTokenTracker accessTokenTracker;
+    /**
+     * The pager adapter, which provides the pages to the view pager widget.
+     */
+    private PagerAdapter mPagerAdapter;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_walkthrough);
 
-        if (!ParseAnonymousUtils.isLinked(ParseUser.getCurrentUser())) {
-            // If current user is NOT anonymous user
-            // Get current user data from Parse.com
-            ParseUser currentUser = ParseUser.getCurrentUser();
-            if (currentUser != null) {
-                // Send logged in users to MyNavigationDrawer.class
-                Account.setUserFBId(Preferences.readFromPreferences(WalkthroughActivity.this, PREF_FILE_NAME, KEY_USER_ID, null));
-                Account.setUserEmail(Preferences.readFromPreferences(WalkthroughActivity.this, PREF_FILE_NAME, KEY_USER_EMAIL, null));
-                Account.setUserName(Preferences.readFromPreferences(WalkthroughActivity.this, PREF_FILE_NAME, KEY_USER_NAME, null));
+        // Instantiate a ViewPager and a PagerAdapter.
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        mPager.setAdapter(mPagerAdapter);
+    }
 
-                startActivity(new Intent(WalkthroughActivity.this, MyNavigationDrawer.class));
-                finish();
-            } else {
-                // Send user to LoginActivity.class
-                Intent intent = new Intent(WalkthroughActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
-            }
+    @Override
+    public void onBackPressed() {
+        if (mPager.getCurrentItem() == 0) {
+            // If the user is currently looking at the first step, allow the system to handle the
+            // Back button. This calls finish() on this activity and pops the back stack.
+            super.onBackPressed();
+        } else {
+            // Otherwise, select the previous step.
+            mPager.setCurrentItem(mPager.getCurrentItem() - 1);
+        }
+    }
+
+    /**
+     * A simple pager adapter that represents 5 ScreenSlidePageFragment objects, in
+     * sequence.
+     */
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+        public ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
         }
 
-        loginButton = (LoginButton) findViewById(R.id.login_button);
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Determine whether the current user is an anonymous user
-                if (ParseAnonymousUtils.isLinked(ParseUser.getCurrentUser())) {
-                    // If user is anonymous, send the user to LoginActivity.class
-                    Intent intent = new Intent(WalkthroughActivity.this, LoginActivity.class);
-                    startActivity(intent);
-                    finish();
-                }
-            }
-        });
+        @Override
+        public Fragment getItem(int position) {
+            return ScreenSlidePageFragment.newInstance(position);
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
     }
 }
