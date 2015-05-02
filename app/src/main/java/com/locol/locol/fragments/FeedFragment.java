8b5a25ca3a8964b700980a232f89564ff5cb459c
Giap Nguyen <nvangiap5894@gmail.com>
2015-05-02 14:11:16 +0700

    Fix bugs

--- app/src/main/java/com/locol/locol/fragments/FeedFragment.java
+++ app/src/main/java/com/locol/locol/fragments/FeedFragment.java
@@ -1,11 +1,8 @@
 package com.locol.locol.fragments;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,10 +14,7 @@ import com.android.volley.toolbox.ImageLoader;
 import com.locol.locol.R;
 import com.locol.locol.adapters.MyRecyclerAdapter;
 import com.locol.locol.adapters.ThingsAdapter;
-import com.locol.locol.application.MainApplication;
-import com.locol.locol.helpers.FeedItemsLoadedListener;
 import com.locol.locol.helpers.Parser;
-import com.locol.locol.networks.Requestor;
 import com.locol.locol.networks.VolleySingleton;
 import com.locol.locol.pojo.FeedItem;
 import com.parse.FindCallback;
@@ -40,7 +34,7 @@ import java.util.List;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class FeedFragment extends Fragment implements FeedItemsLoadedListener, SwipeRefreshLayout.OnRefreshListener {
+public class FeedFragment extends Fragment {
 
     private final static String TAG = "FeedFragment";
     private static final String ARG_PARAM1 = "param1";
@@ -64,23 +58,8 @@ public class FeedFragment extends Fragment implements FeedItemsLoadedListener, S
 
     private ProgressBar progressBar;
 
-    SwipeRefreshLayout mSwipeRefreshLayout;
-
     public FeedFragment() {
         // Required empty public constructor
-    }
-
-
-    private static final String KEY_TITLE = "title";
-    public static FeedFragment newInstance(String title) {
-        FeedFragment f = new FeedFragment();
-
-        Bundle args = new Bundle();
-
-        args.putString(KEY_TITLE, title);
-        f.setArguments(args);
-
-        return (f);
     }
 
     public static FeedFragment newInstance(String param1, String param2) {
@@ -99,41 +78,6 @@ public class FeedFragment extends Fragment implements FeedItemsLoadedListener, S
         final View view = inflater.inflate(R.layout.fragment_feed, container, false);
 
         progressBar = (ProgressBar) view.findViewById(R.id.progress_bar);
-
-        mSwipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipe_refresh_layout);
-        mSwipeRefreshLayout.setOnRefreshListener(this);
-        mSwipeRefreshLayout.setColorSchemeResources(
-                R.color.color_feed_primary
-        );
-        mSwipeRefreshLayout.setRefreshing(true);
-
-//        mRecyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
-//        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
-//        mRecyclerView.setLayoutManager(linearLayoutManager);
-//        mRecyclerView.setOnScrollListener(new EndlessRecyclerOnScrollListener(linearLayoutManager) {
-//            @Override
-//            public void onLoadMore(int currentPage) {
-//                loadFeedItems(currentPage);
-//            }
-//        });
-//
-//        mAdapter = new MyRecyclerAdapter(getActivity(), feedItemList);
-//        mRecyclerView.setAdapter(mAdapter);
-//
-//        if (savedInstanceState != null) {
-//            //if this fragment starts after a rotation or configuration change, load the existing movies from a parcelable
-//            feedItemList = savedInstanceState.getParcelableArrayList(STATE_FEED_ITEMS);
-//        } else {
-//            //if this fragment starts for the first time, load the list of movies from a database
-//            feedItemList = MainApplication.getWritableDatabase().getAllFeedItems();
-//            //if the database is empty, trigger an AsyncTask to download movie list from the web
-//            if (feedItemList.isEmpty()) {
-////                new TaskLoadFeedItems(this).execute(0);
-//                loadFeedItems(0);
-//            }
-//        }
-//
-//        mAdapter.setFeedItems(feedItemList);
 
         final List<String> listCats = new ArrayList<>();
 
@@ -158,90 +102,25 @@ public class FeedFragment extends Fragment implements FeedItemsLoadedListener, S
                 listView = (ListView) view.findViewById(R.id.list_view);
                 listView.setAdapter(adapter);
                 adapter.loadObjects();
-                mSwipeRefreshLayout.setRefreshing(false);
+                adapter.addOnQueryLoadListener(new ParseQueryAdapter.OnQueryLoadListener<ParseObject>() {
+                    @Override
+                    public void onLoading() {
+                        progressBar.setVisibility(View.VISIBLE);
+                    }
+
+                    @Override
+                    public void onLoaded(List<ParseObject> list, Exception e) {
+                        progressBar.setVisibility(View.GONE);
+                    }
+                });
             }
         });
 
         return view;
     }
 
-    private void loadFeedItems(int page) {
-        new TaskLoadFeedItems(this).execute(page);
-    }
-
     private void parseResult(JSONArray response) {
         feedItemList = Parser.parseJSONResponse(response);
-    }
-
-    @Override
-    public void onFeedItemsLoaded(ArrayList<FeedItem> feedItems) {
-        //update the Adapter to contain the new items downloaded from the web
-        if (mSwipeRefreshLayout.isRefreshing()) {
-            mSwipeRefreshLayout.setRefreshing(false);
-        }
-//        mAdapter.setFeedItems(feedItems);
-        adapter.loadObjects();
-    }
-
-    @Override
-    public void onRefresh() {
-//        new TaskLoadFeedItems(this).execute(0);
-//        loadFeedItems(0);
-    }
-
-    private class TaskLoadFeedItems extends AsyncTask<Integer, Void, ArrayList<FeedItem>> {
-        private FeedItemsLoadedListener myComponent;
-        private VolleySingleton volleySingleton;
-        private RequestQueue requestQueue;
-
-        public TaskLoadFeedItems(FeedItemsLoadedListener myComponent) {
-            this.myComponent = myComponent;
-            volleySingleton = VolleySingleton.getInstance();
-            requestQueue = volleySingleton.getRequestQueue();
-        }
-
-        @Override
-        protected void onPreExecute() {
-            if (progressBar.getVisibility() != View.GONE)
-                progressBar.setVisibility(View.VISIBLE);
-        }
-
-        @Override
-        protected ArrayList<FeedItem> doInBackground(Integer... params) {
-            // Eventbrite API
-            // String url = "https://www.eventbriteapi.com/v3/events/search/?venue.city=hanoi&token=DBEK5SF2SVBCTIV52X3L";
-
-            // Self hosting API
-            // String url = "http://104.236.40.66:27080/locoldb/events/_find?batch_size=100";
-
-            // Scrapinghub API
-            String latestJob = "https://storage.scrapinghub.com/jobq/13882/list?count=1";
-            JSONArray jobList = Requestor.sendRequestFeedItems(requestQueue, latestJob, getActivity());
-            String job = Parser.parseLatestJobID(jobList);
-
-            Log.wtf("job id", job);
-
-            String url = "https://storage.scrapinghub.com/items/13882?start=" + job + "/" + 10 * params[0] + "&count=10";
-            JSONArray response = Requestor.sendRequestFeedItems(requestQueue, url, getActivity());
-
-            ArrayList<FeedItem> feedItems = Parser.parseJSONResponse(response);
-
-            MainApplication.getWritableDatabase().insertFeedItems(feedItems, (params[0] == 0));
-
-            feedItemList.addAll(feedItems);
-            // sort feed items by start_date
-//            Collections.sort(feedItemList);
-
-            return feedItemList;
-        }
-
-        @Override
-        protected void onPostExecute(ArrayList<FeedItem> feedItems) {
-            progressBar.setVisibility(View.GONE);
-            if (myComponent != null) {
-                myComponent.onFeedItemsLoaded(feedItems);
-            }
-        }
     }
 
 }
